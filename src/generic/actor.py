2e97a0735aaca61b4634a4d0336c94e49c921c92
lelabo_m <marc.lelabourier@gmail.com>
2017-11-30 12:34:53 +0100

    Updating actor and game.

--- src/generic/actor.py
+++ src/generic/actor.py
@@ -5,3 +5,6 @@ class Actor(object):
 
     def __init__(self):
         self.id = uuid.uuid4()
+
+    def behavior(self):
+        pass
--- src/generic/game.py
+++ src/generic/game.py
@@ -1,19 +1,42 @@
 class Game(object):
 
-    def __init__(self, players):
-        self.turn = 0
-        self.actors = players
+    def __init__(self, player_template, number_of_player, turn=-1):
+        self.__turn = 0
+        self.actors = [player_template() for _ in range(number_of_player)]
         self.events = []
+        self.__turn_events = []
+        self.turn_limit = turn
+
+    @property
+    def turn_number(self):
+        return self.__turn
+
+    def __is_finished(self):
+        return self.turn_number >= self.turn_limit if self.turn_number > 0 else False
 
     def is_finished(self):
+        return True
+
+    def __end(self):
+        if self.is_finished():
+            return True
+        return self.__is_finished()
+
+    def turn(self):
         pass
 
-    def new_turn(self):
-        pass
+    def __new_turn(self):
+        self.events.append(self.__turn_events)
+        self.__turn_events = []
+        self.__turn += 1
+        self.turn()
 
     def loop(self):
         while not self.is_finished():
-            self.new_turn()
+            self.__new_turn()
+
+    def log(self, event):
+        self.__turn_events.append(event)
 
     def analytics(self):
         pass
